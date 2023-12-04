c1aad067bf05553ce1692768a65c0c1f5d89b9f7
Fnapo <36490842+Fnapo@users.noreply.github.com>
2023-12-04 12:30:36 +0100

    Agregar archivos de proyecto.

---
+++ MVCLoteria/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLoteria.Controllers
+{
+    public class HelloWorldController : Controller
+	{
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			_logger.LogInformation("Estoy en la página Index del controlador HelloWorldController.");
+
+			return View();
+		}
+
+		// GET: /HelloWorld/Bienvenido/ 
+		// Requires using System.Text.Encodings.Web;
+		public IActionResult Bienvenido(string name, int numTimes = 1)
+		{
+            _logger.LogInformation("Estoy en la página Bienvenido del controlador HelloWorldController.");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+	}
+}
---
+++ MVCLoteria/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using MVCLoteria.Models;
+using System.Diagnostics;
+
+namespace MVCLoteria.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			_logger.LogInformation("Estoy en la página Index ...");
+
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			_logger.LogInformation("Estoy en la página Privacy ...");
+
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			_logger.LogInformation("Estoy en la página Error ...");
+
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
